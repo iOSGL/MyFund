6ff1fb0cc20d6e7bb5f6e55e99711aa23f726a3c
耿磊 <genglei@shzhanmeng.com>
2020-08-04 16:42:13 +0800

    merge code

---
+++ MyFund/MyFund/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  MyFund
+//
+//  Created by ZM on 2020/8/4.
+//  Copyright © 2020 ZM. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+        var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.backgroundColor = UIColor.white
+        self.window?.rootViewController = ViewController()
+        self.window?.makeKeyAndVisible()
+        return true
+    }
+
+    
+
+}
+
---
+++ MyFund/MyFund/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFund
+//
+//  Created by ZM on 2020/8/4.
+//  Copyright © 2020 ZM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
